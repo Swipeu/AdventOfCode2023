8d5608ca394ffd36532b9d91f38e75ed0fd5559c
Dan <dan.erikson@goldtowngames.com>
2023-12-10 18:52:10 +0100

    day 9

---
+++ day9/part2.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+const file = fs.readFileSync('day9/input.txt', 'utf8');
+
+function getPreviousInSequence(sequence: number[]): number{
+    const diffSequence = getDiffSequence(sequence);
+    if(diffSequence.every(n => n === 0)){
+        return sequence[0];
+    }
+
+    return sequence[0] - getPreviousInSequence(diffSequence);
+}
+
+function getDiffSequence(sequence: number[]){
+    return sequence.slice(1).map((n, index) => n - sequence[index])
+}
+
+let numberArray: number[] = [];
+
+file.split(/\r?\n/).forEach((line, y) => {
+    const originalSequence = line.match(/-?\d+/g).map(n => parseInt(n));
+
+    numberArray.push(getPreviousInSequence(originalSequence));
+
+})
+
+const sum = numberArray.reduce((n, sum) => {
+    return sum + n
+}, 0)
+
+console.log(sum)
